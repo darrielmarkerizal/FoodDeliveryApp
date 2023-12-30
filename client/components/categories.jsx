d91d39cb64608abfc7fa5095e74b56386c2e3d9d
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-31 02:26:55 +0700

    slicing homescreen

--- client/components/categories.jsx
+++ client/components/categories.jsx
@@ -2,8 +2,26 @@ import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
 import { categories } from "../constants";
 
+const Category = ({ category, isActive, onPress }) => {
+  let btnClass = isActive ? "bg-gray-600" : "bg-gray-200";
+  let textClass = isActive ? "font-semibold text-gray-800" : "text-gray-500";
+
+  return (
+    <View className="flex justify-center items-center mr-6">
+      <TouchableOpacity
+        onPress={onPress}
+        className={`p-1 rounded-full shadow ${btnClass}`}
+      >
+        <Image style={{ width: 45, height: 45 }} source={category.image} />
+      </TouchableOpacity>
+      <Text className={`text-sm ${textClass}`}>{category.name}</Text>
+    </View>
+  );
+};
+
 export default function Categories() {
   const [activeCategory, setActiveCategory] = useState(null);
+
   return (
     <View className="mt-4">
       <ScrollView
@@ -12,30 +30,14 @@ export default function Categories() {
         className="overflow-visible"
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
-        {categories.map((category) => {
-          let isActive = category.id == activeCategory;
-          let btnClass = isActive ? " bg-gray-600" : " bg-gray-200";
-          let textClass = isActive
-            ? " font-semibold text-gray-800"
-            : " text-gray-500";
-          return (
-            <View
-              key={category._id}
-              className="flex justify-center items-center mr-6"
-            >
-              <TouchableOpacity
-                onPress={() => setActiveCategory(category.id)}
-                className={"p-1 rounded-full shadow" + btnClass}
-              >
-                <Image
-                  style={{ width: 45, height: 45 }}
-                  source={category.image}
-                />
-              </TouchableOpacity>
-              <Text className={"text-sm " + textClass}>{category.name}</Text>
-            </View>
-          );
-        })}
+        {categories.map((category) => (
+          <Category
+            key={category.id}
+            category={category}
+            isActive={category.id === activeCategory}
+            onPress={() => setActiveCategory(category.id)}
+          />
+        ))}
       </ScrollView>
     </View>
   );
