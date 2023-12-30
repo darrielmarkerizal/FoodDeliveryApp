9f7d4630db5d7b4310806155d46bcc611f9a0995
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-31 01:06:16 +0700

    initiate project

---
+++ navigation.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+import { View, Text } from "react-native";
+import React from "react";
+import RestaurantScreen from "./screens/RestaurantScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
